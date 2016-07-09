3def947527d2247fb7e208c5454dbf54a175ac5f
nharsch <nigelharsch@NIGELs-MBP.home>
2016-07-09 00:35:47 -0400

    clear form on submit

--- app/components/comment_box.jsx
+++ app/components/comment_box.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 var Remarkable = require('remarkable');
 
 
-
 var Comment = React.createClass({
   rawMarkup: function() {
     var md = new Remarkable();
@@ -41,13 +40,42 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
-  render: function() {
-      return (
-            <div className="commentForm">
-              Hello, world! I am a CommentForm.
-            </div>
-          );
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
     }
+    // TODO send request to server
+    this.setState({author: '', text: ''});
+  },
+  render: function() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" 
+          placeholder="Your name" 
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input type="text" 
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
 });
 
 var CommentBox = React.createClass({
